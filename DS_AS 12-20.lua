faea8b4f829ad17e05f1aaa745a25d1d1f2c30b7
max-ri <max_r_@web.de>
2019-08-07 16:36:35 +0200

    first version (from 7-24)

---
+++ DS_AS 12-20.lua
@@ -0,0 +1,134 @@
+local addonName, addon = ...
+Guidelime.registerGuide([[
+[D*ahmp*'s 12-60 guide]
+[GA Alliance]
+[N12-20DS_AS]
+[NX20-22.5AS/ST]
+Should be about 11,75/12 or so
+In rut'theran
+[G36.8,44.3Darkshore]Accept [QA6342 Flight to Auberdine] [ANightElf]
+[F]Fly auberdine
+[G36.6,45.6Darkshore]Accept [QA3524 Washed Ashore p1] 
+[G36.8,44.3Darkshore]Turn in [QT6342 Flight to Auberdine] [ANightElf]
+[G36.8,44.3Darkshore]Accept [QA6343 Return to Nessa] [ANightElf]
+[S]SET HS
+[G37,44.1Darkshore]Accept [QA983 Buzzbox 827] 
+[L37.3,43.6Darkshore]later [QS947 Cave Mushrooms]
+[L37.7,43.4Darkshore]later [QS4811 The Red Crystal]
+[G38.8,43.4Darkshore]Accept [QA2118 Plagued Lands] 
+[G39.4,43.5Darkshore]Accept [QA984 How Big a Threat? p1] 
+[G36.2,43.5Darkshore]Kill [QC983,1 Pygmy Tide Crawler] 
+Kill towards stranded sea creature[OC]
+[G36.4,50.9Darkshore]Loot [QC3524,1 Sea Creature Bones] 
+[G44.1,49.2Darkshore]Go east, capture Rabid Thistle Bear if there helps get level 12
+Run into the furbolg camp to explore
+[G36.6,46.3Darkshore]Turn in [QT983 Buzzbox 827] get 77% xp before turning in if no bear, 70% if bear
+[G36.6,46.3Darkshore]Accept [QA1001 Buzzbox 411] 
+[G36.6,45.6Darkshore]Turn in [QT3524 Washed Ashore p1] 
+[G36.6,45.6Darkshore]Accept [QA4681 Washed Ashore p2] 
+[G38.8,43.4Darkshore]Accept [QA2118 Plagued Lands] doesnt matter yet
+[G39.4,43.5Darkshore]Turn in [QT984 How Big a Threat? p1] 
+[G39.4,43.5Darkshore]Accept [QA985 How Big a Threat? p2] 
+[G39.4,43.5Darkshore]Accept [QA4761 Thundris Windweaver] 
+path to inn
+[G37.7,43.4Darkshore]Accept [QA4811 The Red Crystal] 
+[G37.3,43.6Darkshore]Accept [QA947 Cave Mushrooms] 
+[G37.7,40.7Darkshore]requires 10 cooking grind at least 9 small eggs 
+Ammo/bag
+[G37.4,40.1Darkshore]Turn in [QT4761 Thundris Windweaver] 
+[G37.4,40.1Darkshore]Accept [QA4762 The Cliffspring River] 
+[G37.4,40.1Darkshore]Accept [QA954 Bashal'Aran p1] 
+[G37.4,40.1Darkshore]Accept [QA958 Tools of the Highborne] 
+Priorize killing striders on the way (need 5 meat) to Bashal'Aran
+[G44.2,36.3Darkshore]Turn in [QT954 Bashal'Aran p1] 
+[G44.2,36.3Darkshore]Accept [QA955 Bashal'Aran p2] 
+[G45.2,37.5Darkshore]Kill [QC955,1 Vile Sprite] 
+[G44.2,36.3Darkshore]Turn in [QT955 Bashal'Aran p2] 
+[G44.2,36.3Darkshore]Accept [QA956 Bashal'Aran p3] 
+[G46.7,37.8Darkshore]Kill [QC956,1 Deth'ryll Satyr] 
+[G44.2,36.3Darkshore]Turn in [QT956 Bashal'Aran p3] 
+[G44.2,36.3Darkshore]Accept [QA957 Bashal'Aran p4] 
+Walk to the red crystal and back to town, grind small eggs now
+[H]discover red crystal then hearth to auberdine
+[G37.7,43.4Darkshore]Turn in [QT4811 The Red Crystal] 
+[G37.7,43.4Darkshore]Accept [QA4812 As Water Cascades] FILL tube!!!!!!
+Continue back to red crysal
+[G47.3,48.7Darkshore]Turn in [QT4812 As Water Cascades] at red crystal
+[G47.3,48.7Darkshore]Accept [QA4813 The Fragments Within] at red crystal
+Grind back to town should be done with striders
+[G37.7,43.4Darkshore]Turn in [QT4813 The Fragments Within] staff
+Go cook the herb baked eggs to get 10 ocoking
+[G37.7,40.7Darkshore]Accept [QA2178 Easy Strider Living] 
+[G37.7,40.7Darkshore]Turn in [QT2178 Easy Strider Living] 
+[G35.7,43.7Darkshore]Accept [QA963 For Love Eternal] 
+Kill threshers [QS1001 Buzzbox 411]
+[G31.8,46.3Darkshore]Loot [QC4681,1 Sea Turtle Remains] 
+Efficient move towards ghosts killing some furbolgs on the way
+[G40.3,59.7Darkshore]Accept [QA953 The Fall of Ameth'Aran] 
+[G42.5,61.3Darkshore]Kill [QC958,1 Writhing Highborne] grind these ghosts
+[G41.8,60.7Darkshore]Kill [QC963,1 Anaya Dawnrunner] 
+read both lay/fall [QS953 The Fall of Ameth'Aran]
+USE ANCIENT MOONSTONE SEAL
+hit 14,5 here MINIMUM without turn ins to achieve appropriate level curve (grinding ghosts until 14 then furbolgs to even 15 if no contestion)
+[G39.7,54.4Darkshore]Kill [QC985,2 Blackwood Windtalker] finish these after hitting level 14
+[G39.6,54.4Darkshore]Kill [QC985,1 Blackwood Pathfinder] 
+Capture a bear [QS2118 Plagued Lands]
+[G40.3,59.7Darkshore]Turn in [QT953 The Fall of Ameth'Aran] 
+Efficient move back towards camp grinding
+[G35.7,43.7Darkshore]Turn in [QT963 For Love Eternal] Sell
+[G39.4,43.5Darkshore]Turn in [QT985 How Big a Threat? p2] 
+[G38.8,43.4Darkshore]Turn in [QT2118 Plagued Lands] if not already
+[G38.8,43.4Darkshore]Accept [QA2138 Cleansing of the Infected] 
+[G39.4,43.5Darkshore]Accept [QA986 A Lost Master p1] 
+[G39,43.6Darkshore]Accept [QA965 The Tower of Althalaxx p1] 
+[G38.1,41.2Darkshore]Accept [QA982 Deep Ocean, Vast Sea] 
+[V]Ammo
+[G37.4,40.1Darkshore]Turn in [QT958 Tools of the Highborne] 
+Jump into the water(north)
+[G33.8,42.1Darkshore]Kill [QC1001,1 Darkshore Thresher] if need more eyes (coords wrong)
+[G38.2,28.8Darkshore]Loot [QC982,1 Silver Dawning's Lockbox] Back bottom of the ship
+[G39.6,27.5Darkshore]Loot [QC982,2 Mist Veil's Lockbox] Back bottom of the ship
+[G42,28.6Darkshore]Turn in [QT1001 Buzzbox 411] 
+[G42,28.6Darkshore]Accept [QA1002 Buzzbox 323] 
+[G41.9,31.5Darkshore]Loot [QA4723 ] Loot Sea creature nearby
+go north (filler cat + bears)
+[G44.2,20.6Darkshore]Loot [QA4725 ] Loot Sea creature nearby
+Move towards river/waterfall & collect sample + bear filler + 4 spawn moonstalker
+[G53.1,18.1Darkshore]Loot [QA4727 ] Loot Sea creature nearby using time to regen + save 1 bear to run out (kite)
+[G55,24.9Darkshore]Turn in [QT965 The Tower of Althalaxx p1] 
+[G55,24.9Darkshore]Accept [QA966 The Tower of Althalaxx p2] 
+[G56.3,26.7Darkshore]Kill [QC966,1 Dark Strand Fanatic] 
+[G55,24.9Darkshore]Turn in [QT966 The Tower of Althalaxx p2] 
+[G55,24.9Darkshore]Accept [QA967 The Tower of Althalaxx p3] 
+Run towards mushroom q (filler moonstalker + bears)
+Finish moonstalker fangs and Cleansing of the Infected
+[G42,28.6Darkshore]Turn in [QT1002 Buzzbox 323] Not 100% neccessary can turn in with next north pass
+[G42,28.6Darkshore]Accept [QA1003 Buzzbox 525] 
+[G55.1,34.6Darkshore]Loot [QC947,1 Scaber Stalk] 
+[G56.3,34.9Darkshore]Loot [QC947,2 Death Cap] 
+efficient grind to bashal [OC]
+[G44.2,36.3Darkshore]Turn in [QT957 Bashal'Aran p4] 
+target high level striders and melee kill back to town, kite fill otherwise
+[G38.8,43.4Darkshore]Turn in [QT2138 Cleansing of the Infected] 
+[G38.8,43.4Darkshore]Accept [QA2139 Tharnariun's Hope] 
+[G38.1,41.2Darkshore]Turn in [QT982 Deep Ocean, Vast Sea] 
+[G37.4,40.5Darkshore][V]Ammo
+[G37.4,40.1Darkshore]Turn in [QT4762 The Cliffspring River] 
+[G37.4,40.1Darkshore]Accept [QA4763 The Blackwood Corrupted] 
+[L37.4,41.8Darkshore]later [QS729 The Absent Minded Prospector p1]
+[G37.3,43.6Darkshore]Turn in [QT947 Cave Mushrooms] 
+[G37.3,43.6Darkshore]Accept [QA948 Onu] 
+Use bowl in moonwell
+[G37.2,44.2Darkshore]Accept [QA4740 WANTED: Murkdeep!] 
+[G36.6,45.6Darkshore]Turn in [QT4731 Beached Sea Creature] 
+[G36.6,45.6Darkshore]Turn in [QT4732 Beached Sea Turtle] 
+[G36.6,45.6Darkshore]Turn in [QT4733 Beached Sea Turtle] 
+[F]Level 16,25 fly to darn
+[G56.3,92.4Teldrassil]Turn in [QT4733 Return to Nessa] 
+[G23.7,64.5Teldrassil]Accept [QA4733 Trouble In Darkshore?] 
+TOTAL GOLD REQ FOR DARN: 1g 35s (if below this number, then skip arcane shot which brings down to 1g,24 and if REALLY low skip level 20 bow)
+[V]hunter Buy both level 16 and 20 bows from fletcher, 91s 45c req
+[V]Buy 20 level 10 meat for pet :)
+[T]Train First Aid Heavy Linen
+[T]Train: Track Beast Mend Pet Arcane Shot Distracting Shot Wing Clip Immolation Trap
+[T]trainer > HS auberdine
